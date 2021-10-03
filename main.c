a134629308b61767d79f51bfd854ac1d7ce28434
Mergen Imeev <imeevma@gmail.com>
2021-10-03 22:27:30 +0300

    main: prepare main() for new operations
    This patch adds new format for arguments of the program.

--- main.c
+++ main.c
@@ -1,12 +1,65 @@
-#include "main.h"
+#include <string.h>
 #include <stdio.h>
+
+#include "main.h"
+
+enum mode {
+	NOTHING = 0,
+	CONVERTER = 1,
+};
 
 int
 main(int argc, const char **argv)
 {
-	if (argc != 3) {
-		fprintf(stderr, "Please provide input and output files.\n");
+	if (argc < 2) {
+		fprintf(stderr, "Too little arguments.\n");
 		return -1;
 	}
-	return converter(argv[1], argv[2]);
+	enum mode mode = NOTHING;
+	const char *fin = NULL;
+	const char *fout = NULL;
+	for (int i = 1; i < argc; ++i) {
+		if (argv[i][0] != '-' || strlen(argv[i]) != 2) {
+			fprintf(stderr, "Wrong argument: %s.\n", argv[i]);
+			return -1;
+		}
+		switch (argv[i][1]) {
+		case 'c':
+			if (i + 1 >= argc) {
+				fprintf(stderr, "Input file name missing.\n");
+				return -1;
+			}
+			++i;
+			fin = argv[i];
+			if (mode != NOTHING) {
+				fprintf(stderr, "Too many modes.\n");
+				return -1;
+			}
+			mode = CONVERTER;
+			break;
+		case 'o':
+			if (i + 1 >= argc) {
+				fprintf(stderr, "Output file name missing.\n");
+				return -1;
+			}
+			++i;
+			fout = argv[i];
+			break;
+		}
+	}
+
+	switch (mode) {
+	case CONVERTER:
+		if (fin == NULL) {
+			fprintf(stderr, "Input file name missing.\n");
+			return -1;
+		}
+		if (fout == NULL)
+			fout = "converter_output";
+		return converter(fin, fout);
+	default:
+		fprintf(stderr, "Mode does not defined.\n");
+		return -1;
+	}
+	return 0;
 }
